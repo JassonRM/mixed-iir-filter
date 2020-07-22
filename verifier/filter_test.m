6011313dcf4faaf364399dcdd48eee92bd7cf940
Jasson Rodriguez <jassonrm@icloud.com>
2020-07-22 11:56:39 -0600

    Change filter cutoff frequency

--- verifier/filter_test.m
+++ verifier/filter_test.m
@@ -1,7 +1,8 @@
 function signal_out = filter_test(signal_in)
  pkg load signal
-
- [b, a] = butter(1, 50 / 800);
+ fc = 200; 
+ fs = 40000;  
+ [b,a] = butter(1,fc/(fs/2), "high")
  
  filtered = filter(b, a, signal_in);
 
@@ -9,8 +10,11 @@ function signal_out = filter_test(signal_in)
  w1 = 0;
  x = signal_in;
  for n = 1:size(signal_in, 1)
-   w2 = x(n) - a(2) * w1;
-   y = b(1) * w2 + b(2) * w1;
+   a1w = - a(2) * w1;
+   w2 = x(n) + a1w;
+   b0w = b(1) * w2;
+   b1w = b(2) * w1;
+   y = b0w + b1w; 
    w1 = w2;
    myfilter = [myfilter; y];
  endfor
