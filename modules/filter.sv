7f634fd9cd157921b8f68625e79d03cc4a3c8543
Jasson Rodriguez <jassonrm@icloud.com>
2020-07-22 12:14:20 -0600

    Add offset to filter

--- modules/filter.sv
+++ modules/filter.sv
@@ -2,12 +2,13 @@ module filter(input logic clk, rst,
 					input logic[15:0] x, a1, b0, b1,
 					output logic[15:0] y);
 					
-logic[15:0] w1, w2, a1w, b0w, b1w;
+logic[15:0] offset, w1, w2, a1w, b0w, b1w;
 
+adder Adder0(x, 'b1000001010000000, offset);
 
 multiply Multiply1({(a1[15] ^ 1), a1[14:0]}, w1, a1w);
 
-adder Adder1(x, a1w, w2);
+adder Adder1(offset, a1w, w2);
 
 multiply Multiply2(b0, w2, b0w);
 
