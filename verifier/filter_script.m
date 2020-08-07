0dbe9371a17c731ce26ad4a4ba950936ab52b93d
Marco <m.herrera0799@gmail.com>
2020-08-06 18:09:03 -0600

    Update filter output files

--- verifier/filter_script.m
+++ verifier/filter_script.m
@@ -4,31 +4,54 @@
 [modelsim1] = process_output("/home/marco/Documents/Projects/mixed-iir-filter/verifier/low_pass.txt", 15);
 [modelsim2] = process_output("/home/marco/Documents/Projects/mixed-iir-filter/verifier/high_pass.txt", 15);
 
-clf
+##clf
 subplot ( 3, 3, 1)
+subplot ( 2, 1, 1)
 plot(signal2(:))
 title("Original")
 
-subplot ( 3, 3, 4)
-plot(filtered1(:))
-title("Octave low filter")
+subplot ( 2, 1, 2)
+plot(signal2(1:5000))
+title("Original")
 
-subplot ( 3, 3, 5)
-plot(low_out2(:))
-title("My Octave low filter")
 
-subplot ( 3, 3, 6)
-plot(modelsim1(:))
-title("My hardware low Filter")
+##subplot ( 3, 3, 4)
+##plot(filtered1(:))
+##title("Octave low filter")
+##
 
-subplot ( 3, 3, 7)
-plot(filtered2(:))
-title("Octave high filter")
+##subplot ( 3, 3, 5)
+##plot(low_out2(:))
+##title("My Octave low filter")
+##
 
-subplot ( 3, 3, 8)
-plot(high_out2(:))
-title("My Octave high filter")
+##subplot ( 3, 3, 6)
+##plot(modelsim1(:))
+##title("My hardware low Filter")
+##
 
-subplot ( 3, 3, 9)
-plot(modelsim2(:))
-title("My hardware high filter")
+##subplot ( 3, 3, 7)
+####subplot ( 2, 1, 1)
+##plot(filtered2(:))
+##title("Octave high filter")
+##
+
+####subplot ( 2, 1, 2)
+####plot(filtered2(1:500))
+####title("Octave high filter")
+##
+
+##subplot ( 3, 3, 8)
+##plot(high_out2(:))
+##title("My Octave high filter")
+##
+
+##subplot ( 3, 3, 9)
+####subplot ( 2, 1, 1)
+##plot(modelsim2(:))
+##title("My hardware high filter")
+##
+
+####subplot ( 2, 1, 2)
+####plot(modelsim2(1:500))
+####title("My hardware high filter")
